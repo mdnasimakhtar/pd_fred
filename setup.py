ec024904fe35c5deef9dfcc8143a28fa899fc101
Md Nasim Akhtar <62100924+mdnasimakhtar@users.noreply.github.com>
2024-10-24 18:43:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pd_fred',
-    version='0.1.0',
+    version='0.1.1',
     author='Your Name',
     author_email='your.email@example.com',
     description='Python package to calculate Probability of Default using FRED data.',
